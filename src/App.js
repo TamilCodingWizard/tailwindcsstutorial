7e013fdde0120e2c985d6fb5cbd31ceb65d73606
tamilCodingWizard <tamilcodingwizard@gmail.com>
2023-02-09 22:31:38 +0000

    video-02

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <span className="text-5xl font-bold text-green-500">Fruit</span> <span className="text-4xl font-light">Shop</span>
+
+    <div>
+      <span className="text-stone-500 text-4xl font-display">Season Specials</span>
+    </div>
+    <div>
+      <span className="text-stone-500 text-4xl font-display">Find us on</span>
+    </div>
+    </>
   );
 }
 
